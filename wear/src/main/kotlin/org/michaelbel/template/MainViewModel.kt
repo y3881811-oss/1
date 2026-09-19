0bce610f91f47ecd9160418df78abc36a59da9c9
y3881811-oss <e13768782404@outlook.com>
2026-09-19 21:58:30 +0800

    Delete wear directory

--- wear/src/main/kotlin/org/michaelbel/template/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package org.michaelbel.template
-
-import org.michaelbel.shared.viewmodel.BaseViewModel
-import org.michaelbel.shared.interactor.AppInteractor
-
-class MainViewModel(
-    private val appInteractor: AppInteractor
-): BaseViewModel()
