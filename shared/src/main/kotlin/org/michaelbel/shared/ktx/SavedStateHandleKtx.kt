773f320d9cf24b592487230aa45edf379a73c395
y3881811-oss <e13768782404@outlook.com>
2026-09-19 21:58:02 +0800

    Delete shared directory

--- shared/src/main/kotlin/org/michaelbel/shared/ktx/SavedStateHandleKtx.kt
+++
@@ -1,7 +0,0 @@
-@file:Suppress("unused")
-
-package org.michaelbel.shared.ktx
-
-import androidx.lifecycle.SavedStateHandle
-
-fun <T> SavedStateHandle.require(key: String): T = requireNotNull(this[key])
